3490cace92b3129fb78133f37ead510166ef3af4
Gaurav Rehan <gaurav.rehan@gmail.com>
2016-08-14 16:08:20 +0530

    Sping MCV Webapp - Views module

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -13,7 +13,16 @@ public class MinutesController {
 	public String addMinutes(@ModelAttribute ("exercise") Exercise exercise)
 	{
 		System.out.println("exercise " + exercise.getMinutes());
+		return "addeMinutes";
+		//we can use redirect: also in order to redirect instead of forward
+		//return "forward:addMoreMinutes.html";
+		//return "redirect:addMoreMinutes.html";
+	}
+	
+	@RequestMapping(value="/addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute ("exercise") Exercise exercise)
+	{
+		System.out.println("more exercise " + exercise.getMinutes());
 		return "addMinutes";
-		
 	}
 }
